9ab928b7fdb6a96080bc7abb97da017879c59c47
Shals7 <shalinisssk@gmail.com>
2016-10-13 12:11:44 +0530

    Update DemoA.java

--- src/demo_git/DemoA.java
+++ src/demo_git/DemoA.java
@@ -5,5 +5,6 @@ public class DemoA
 	public static void main(String[] args) 
 	{
 		System.out.println("Hello... Welcome to GitHub from Eclipse");
+		System.out.println("Hello... Welcome to Github from Github Repository");
 	}
 }
